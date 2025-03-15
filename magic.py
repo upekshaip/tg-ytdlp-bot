a55bb49fd548f10c2d6b3183c91576dc591aa6f9
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-03-15 17:11:02 +0300

    Update magic.py

--- magic.py
+++ magic.py
@@ -1503,8 +1503,8 @@ def down_and_up(app, message, url, playlist_name, video_count, video_start_with)
                     message,
                     f"❌ Failed to download video: {error_message}\n────────────────\n"
                     "> Check [here](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md) if your site supported\n"
-                    "> You may need `cookie` for downloading this video.\n"
-                    "> For Youtube - get `cookie` via /download_cookie command. For any other supported site - send your own cookie ([guide1](https://t.me/c/2303231066/18)) ([guide2](https://t.me/c/2303231066/22)) and after that send your video link again."
+                    "> You may need `cookie` for downloading this video. First, clean your workspace via **/clean** command\n"
+                    "> For Youtube - get `cookie` via **/download_cookie** command. For any other supported site - send your own cookie ([guide1](https://t.me/c/2303231066/18)) ([guide2](https://t.me/c/2303231066/22)) and after that send your video link again."
                 )
                 continue  # Move to the Next Video If Available
 
