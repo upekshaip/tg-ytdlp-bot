d9fb8c2ae4a3103b00f39d754ad98dca57803baf
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-06-26 17:23:21 +0300

    Update _config.py

--- _config.py
+++ _config.py
@@ -33,6 +33,7 @@ class Config(object):
     # your firebase DB path
     BOT_DB_PATH = f"bot/{BOT_NAME}/"
     VIDEO_CACHE_DB_PATH = f"bot/video_cache"
+    PLAYLIST_CACHE_DB_PATH = f"bot/video_cache/playlists"
     # Firebase Config - Required (str for all)
     FIREBASE_USER = "YOUR@E.MAIL"
     FIREBASE_PASSWORD = "YOUR_PASSWORD"
