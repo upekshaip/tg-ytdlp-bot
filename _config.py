ea8661f4bbdff0f2649c6ea8a60ba60da44464fc
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-07-18 18:56:13 +0300

    Update _config.py

--- _config.py
+++ _config.py
@@ -132,6 +132,8 @@ To download playlists send its URL with <code>*start*end</code> ranges in the en
 🟧 <b>Pornhub models:</b>
 <code>https://www.pornhub.org/model/MODEL_NAME*1*2</code>
 (downloads first 2 video from model profile)
+<code>https://it.pornhub.com/video/search?search=YOUR_PROMPT*1*3</code>
+(downloads first 3 video from search results by your prompt)
 
 and so on...
 see <a href="https://raw.githubusercontent.com/yt-dlp/yt-dlp/refs/heads/master/supportedsites.md">supported sites list</a>
